a0bd6e20b4cc043b71570528ee2e2b0429429591
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2012-05-13 16:34:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityStream::Application.config.secret_token = 'a303f0f6959673ea6ac35675d493629dda53ec93a917296da5d7677311d16ec2f0ec93b39e9999ceebe74c78f7b30ba7354a94b785797cc04a938b0966e7ccfa'
